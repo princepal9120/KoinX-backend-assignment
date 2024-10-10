6a424349f3f06f288ebdc654f6dd594f4d016986
Prince Pal <princepal9120@gmail.com>
2024-10-11 00:12:48 +0530

    correct db error

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,19 @@ import mongoose from 'mongoose';
 
 const connectDatabase = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,       
+      useUnifiedTopology: true,      
+     
+      writeConcern: {
+        w: 'majority',               
+        wtimeout: 5000,             
+      }
+    });
     console.log('MongoDB connected');
   } catch (err) {
     console.error('MongoDB connection error:', err);
-    process.exit(1);
+    process.exit(1); // Exit the process with an error code if the connection fails
   }
 };
 
